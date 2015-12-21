195a457cba0eebca1aa1ad57449e23e787850749
lovecc0923 <luorongchun@gmail.com>
2015-12-21 20:39:21 +0800

    Auth by MongoDB

--- src/emqttd_mongodb_app.erl
+++ src/emqttd_mongodb_app.erl
@@ -10,9 +10,13 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    Env = application:get_all_env(),
-    emqttd_mongodb:onload(Env),
-    emqttd_mongodb_sup:start_link().
+%% 	{ok, Database}  = application:get_env(?MODULE, database),
+%% 	{ok, Collection}  = application:get_env(?MODULE, collection),
+%% 	{ok, HashType} = application:get_env(?MODULE, password_hash),
+  ok = emqttd_access_control:register_mod(auth, emqttd_auth_mongodb, {<<"db0">>, <<"mqtt_user">>, sha256}),
+  Env = application:get_all_env(),
+  emqttd_mongodb:onload(Env),
+  emqttd_mongodb_sup:start_link().
 
 stop(_State) ->
-    emqttd_mongodb:onunload(), ok.
+  emqttd_mongodb:onunload(), ok.
