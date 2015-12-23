37f9343b67d29cdb52b287c8530714350ee36018
luorongchun <944398029@qq.com>
2015-12-23 18:19:42 +0800

    Add TopicHistory

--- src/emqttd_mongodb.erl
+++ src/emqttd_mongodb.erl
@@ -36,8 +36,10 @@ onunload() ->
 %%    sys    = false  :: boolean(),         %% $SYS flag
 %%    payload         :: binary(),          %% Payload
 %%    timestamp       :: erlang:timestamp() %% os:timestamp
-store(#mqtt_message{msgid = MsgId, pktid = PktId, payload = Payload, timestamp = Timestamp}) ->
-  [{_, Body}, {_, Direction}, {_, From}, {_, Nickname}, {_, SubType}, {_, Time}, {_, To}, {_, Topic}, {_, Type}] = jsx:decode(Payload),
+store(#mqtt_message{msgid = MsgId, pktid = PktId, from = From, topic = Topic, payload = Payload, timestamp = Timestamp}) ->
+%%[{_, Body}, {_, Direction}, {_, From}, {_, Nickname}, {_, SubType}, {_, Time}, {_, To}, {_, Topic}, {_, Type}] = jsx:decode(Payload),
+  [{_, Body}, {_, Direction}, {_, Nickname}, {_, SubType}, {_, Time}, {_, To}, {_, Type}] = jsx:decode(Payload),
+  %% 聊天记录存储
   Msg1 = #{
     <<"body">> => Body,
     <<"direction">> => Direction,
@@ -61,4 +63,39 @@ store(#mqtt_message{msgid = MsgId, pktid = PktId, payload = Payload, timestamp =
     <<"type">> => Type
   },
   {ok, Connection} = mongo:connect([{database, <<"db0">>}]),
-  mongo:insert(Connection, <<"mqtt_message">>, [Msg1, Msg2]).
+  mongo:insert(Connection, <<"Message">>, [Msg1, Msg2]).
+  %% 聊天记录存储
+  
+  %% 聊天列表记录存储
+  UserId = list_to_integer(From),
+  Map1 = #{
+    <<"type">> 		=> type(Topic),	%% 消息类型
+    <<"userId">> 	=> UserId,		%% 消息发送者
+    <<"id">> 		=> Topic,		%% 主题
+    <<"name">> 		=> Nickname,	%% 聊天记录名字
+    <<"desc">> 		=> Body,		%% 聊天记录最后一条消息
+    <<"time">> 		=> timestamp(),	%% 聊天记录最后一条消息发送时间
+    <<"count">> 	=> 0			%% 未读消息数
+  },
+  
+  %% 聊天列表记录存储
+  
+%% 获取用户Id
+id(Topic) ->
+	Index = string:chr(Topic, 47),
+	string:substr(Topic, Index + 1).
+
+%% 获取类型
+type(Topic) ->
+	Index = string:chr(Topic, 47),
+	case string:substr(Topic, 1, Index - 1) of
+		"USER" -> 1;
+		"GROUP" -> 2;
+		"SYSTEM" -> 3;
+		_ -> 0
+	end.
+
+%% 获取当前时间秒数
+timestamp() -> erlang:now() / 1000.  
+  
+  
