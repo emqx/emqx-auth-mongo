5e626b4e78ab5e9137353f68cfc23c31451499dd
Feng Lee <ery.lee@gmail.com>
2016-05-30 16:02:36 +0800

    1.1 - Support ACL

--- src/emqttd_acl_mongo.erl
+++ src/emqttd_acl_mongo.erl
@@ -19,22 +19,62 @@
 
 -behaviour(emqttd_acl_mod).
 
+-include("emqttd_plugin_mongo.hrl").
+
 -include("../../../include/emqttd.hrl").
 
 %% ACL callbacks
 -export([init/1, check_acl/2, reload_acl/1, description/0]).
 
--record(state, {acl_nomatch}).
+-record(state, {superquery, aclquery, nomatch}).
 
-init(_Args) ->
-    {ok, #state{}}.
+init({SuperQuery, AclQuery, AclNomatch}) ->
+    {ok, #state{superquery = SuperQuery, aclquery = AclQuery, nomatch = AclNomatch}}.
 
-check_acl({Client, PubSub, Topic}, State = #state{acl_nomatch = Default}) ->
-    Default.
+check_acl({#mqtt_client{username = <<$$, _/binary>>}, _PubSub, _Topic}, _State) ->
+    {error, bad_username};
+
+check_acl({Client, PubSub, Topic}, #state{superquery = SuperQuery,
+                                          aclquery   = AclQuery,
+                                          nomatch    = Default}) ->
+    case emqttd_plugin_mongo:is_superuser(SuperQuery, Client) of
+        false -> #aclquery{collection = Coll, selector = Selector} = AclQuery,
+                 Row = emqttd_plugin_mongo:query(Coll, emqttd_plugin_mongo:replvar(Selector, Client)),
+                 io:format("AclRow: ~p~n", [Row]),
+                 case match(Client, Topic, topics(PubSub, Row)) of
+                     matched -> allow;
+                     nomatch -> Default
+                 end;
+        true  -> Default
+    end.
+
+match(_Client, _Topic, []) ->
+    nomatch;
+match(Client, Topic, [TopicFilter|More]) ->
+    case emqttd_topic:match(Topic, feedvar(Client, TopicFilter)) of
+        true  -> matched;
+        false -> match(Client, Topic, More)
+    end.
+
+topics(publish, Row) ->
+    lists:umerge(maps:get(<<"publish">>, Row, []), maps:get(<<"pubsub">>, Row, []));
+
+topics(subscribe, Row) ->
+    lists:umerge(maps:get(<<"subscribe">>, Row, []), maps:get(<<"pubsub">>, Row, [])).
+
+feedvar(#mqtt_client{client_id = ClientId, username = Username}, Str) ->
+    lists:foldl(fun({Var, Val}, Acc) ->
+                    feedvar(Acc, Var, Val)
+                end, Str, [{"%u", Username}, {"%c", ClientId}]).
+
+feedvar(Str, _Var, undefined) ->
+    Str;
+feedvar(Str, Var, Val) ->
+    re:replace(Str, Var, Val, [global, {return, binary}]).
 
 reload_acl(_State) ->
     ok.
 
 description() ->
-    "MongoDB ACL Module".
+    "ACL with MongoDB".
 
