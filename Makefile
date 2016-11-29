1adc08249eefbdd3e886289e447504559aab54e1
Feng Lee <ery.lee@gmail.com>
2016-11-29 16:54:53 +0800

    2.0.1

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 PROJECT = emq_auth_mongo
 PROJECT_DESCRIPTION = Authentication/ACL with MongoDB
-PROJECT_VERSION = 2.0
+PROJECT_VERSION = 2.0.1
 
 DEPS = mongodb ecpool
 
--- emq_auth_mongo.d
+++
@@ -1,6 +0,0 @@
-src/emq_acl_mongo.erl:: include/emq_auth_mongo.hrl; @touch $@
-src/emq_auth_mongo.erl:: include/emq_auth_mongo.hrl; @touch $@
-src/emq_auth_mongo_app.erl:: include/emq_auth_mongo.hrl; @touch $@
-src/emq_auth_mongo_sup.erl:: include/emq_auth_mongo.hrl; @touch $@
-
-COMPILE_FIRST +=
