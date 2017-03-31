278300c3d15fe57d0038bd39645b4b0e287d522a
HuangDan <huangd@emqtt.io>
2017-03-31 10:41:39 +0800

    Format code

--- src/emq_auth_mongo.erl
+++ src/emq_auth_mongo.erl
@@ -59,7 +59,7 @@ check(Client, Password, #state{authquery = AuthQuery, superquery = SuperQuery}) 
 check_pass(PassHash, Password, HashType) ->
     check_pass(PassHash, hash(HashType, Password)).
 check_pass(PassHash, Salt, Password, {pbkdf2, Macfun, Iterations, Dklen}) ->
-    check_pass(PassHash,hash(pbkdf2,{Salt,Password, Macfun, Iterations, Dklen}));
+    check_pass(PassHash, hash(pbkdf2, {Salt, Password, Macfun, Iterations, Dklen}));
 check_pass(PassHash, Salt, Password, {salt, HashType}) ->
     check_pass(PassHash, hash(HashType, <<Salt/binary, Password/binary>>));
 check_pass(PassHash, Salt, Password, {HashType, salt}) ->
