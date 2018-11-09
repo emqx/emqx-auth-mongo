293385f09283ffba09ec5ca5a2a36e7b988415ee
terry-xiaoyu <506895667@qq.com>
2018-11-09 21:27:41 +0800

    Read config files from deps/ for test suits

--- test/emqx_auth_mongo_SUITE.erl
+++ test/emqx_auth_mongo_SUITE.erl
@@ -49,8 +49,13 @@ groups() ->
      {auth_mongo_config, [sequence], [server_config]}].
 
 init_per_suite(Config) ->
-    DataDir = proplists:get_value(data_dir, Config),
-    Apps = [start_apps(App, DataDir) || App <- [emqx, emqx_auth_mongo]],
+    Apps =
+      [start_apps(App, {SchemaFile, ConfigFile}) ||
+        {App, SchemaFile, ConfigFile}
+            <- [{emqx, local_path("deps/emqx/priv/emqx.schema"),
+                       local_path("deps/emqx/etc/emqx.conf")},
+                {emqx_auth_mongo, local_path("priv/emqx_auth_mongo.schema"),
+                                  local_path("etc/emqx_auth_mongo.conf")}]],
     ct:log("Apps:~p~n", [Apps]),
 
     {ok, Connection} = ecpool_worker:client(gproc_pool:pick_worker({ecpool, emqx_auth_mongo})),
@@ -64,6 +69,34 @@ end_per_suite(Config) ->
     mongo_api:delete(Connection, AclCollection, {}),
     application:stop(emqx_auth_mongo),
     application:stop(emqttd).
+
+get_base_dir() ->
+    {file, Here} = code:is_loaded(?MODULE),
+    filename:dirname(filename:dirname(Here)).
+
+local_path(RelativePath) ->
+    filename:join([get_base_dir(), RelativePath]).
+
+start_apps(App, {SchemaFile, ConfigFile}) ->
+    read_schema_configs(App, {SchemaFile, ConfigFile}),
+    set_special_configs(App),
+    application:ensure_all_started(App).
+
+read_schema_configs(App, {SchemaFile, ConfigFile}) ->
+    ct:pal("Read configs - SchemaFile: ~p, ConfigFile: ~p", [SchemaFile, ConfigFile]),
+    Schema = cuttlefish_schema:files([SchemaFile]),
+    Conf = conf_parse:file(ConfigFile),
+    NewConfig = cuttlefish_generator:map(Schema, Conf),
+    Vals = proplists:get_value(App, NewConfig, []),
+    [application:set_env(App, Par, Value) || {Par, Value} <- Vals].
+
+set_special_configs(emqx) ->
+    application:set_env(emqx, allow_anonymous, false),
+    application:set_env(emqx, enable_acl_cache, false),
+    application:set_env(emqx, plugins_loaded_file,
+                        local_path("deps/emqx/test/emqx_SUITE_data/loaded_plugins"));
+set_special_configs(_App) ->
+    ok.
 
 check_auth(_Config) ->
     {ok, Connection} = ?POOL(?APP),
@@ -238,14 +271,6 @@ find(Connection, Collection, Selector, Projector) ->
     Result = mc_cursor:rest(Cursor),
     mc_cursor:close(Cursor),
     Result.
-
-start_apps(App, DataDir) ->
-    Schema = cuttlefish_schema:files([filename:join([DataDir, atom_to_list(App) ++ ".schema"])]),
-    Conf = conf_parse:file(filename:join([DataDir, atom_to_list(App) ++ ".conf"])),
-    NewConfig = cuttlefish_generator:map(Schema, Conf),
-    Vals = proplists:get_value(App, NewConfig),
-    [application:set_env(App, Par, Value) || {Par, Value} <- Vals],
-    application:ensure_all_started(App).
 
 reload({Par, Vals}) when is_list(Vals) ->
     application:stop(?APP),
