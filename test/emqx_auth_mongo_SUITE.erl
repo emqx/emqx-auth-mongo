4c4215b0fc73f9c78d0852e4b96505295243a8bd
terry-xiaoyu <506895667@qq.com>
2019-03-16 17:31:52 +0800

    Revert test case list_auth

--- test/emqx_auth_mongo_SUITE.erl
+++ test/emqx_auth_mongo_SUITE.erl
@@ -44,7 +44,7 @@ all() ->
      {group, auth_mongo_config}].
 
 groups() ->
-    [{emqx_auth_mongo_auth, [sequence], [check_auth]},
+    [{emqx_auth_mongo_auth, [sequence], [check_auth, list_auth]},
      {emqx_auth_mongo_acl, [sequence], [check_acl, acl_super]},
      {auth_mongo_config, [sequence], [server_config]}].
 
@@ -143,7 +143,7 @@ list_auth(_Config) ->
     application:start(emqx_auth_username),
     emqx_auth_username:add_user(<<"user1">>, <<"password1">>),
     User1 = #{client_id => <<"client1">>, username => <<"user1">>},
-    ok = emqx_access_control:authenticate(User1#{password => <<"password1">>}),
+    {ok, _} = emqx_access_control:authenticate(User1#{password => <<"password1">>}),
     reload({auth_query, [{password_hash, plain}, {password_field, [<<"password">>]}]}),
     Plain = #{client_id => <<"client1">>, username => <<"plain">>},
     {ok, #{is_superuser := true}} = emqx_access_control:authenticate(Plain#{password => <<"plain">>}),
