7cfeb6b1ef93cd839806d27a3b06905daad6d544
zhanghongtong <zhanghongtong@foxmail.com>
2020-03-27 10:41:40 +0800

    Delete duplicate statements

--- src/emqx_auth_mongo.erl
+++ src/emqx_auth_mongo.erl
@@ -45,10 +45,6 @@ check(ClientInfo = #{password := Password}, AuthResult,
                hash = HashType, selector = Selector} = AuthQuery,
     case query(Collection, maps:from_list(replvars(Selector, ClientInfo))) of
         undefined -> emqx_metrics:inc(?AUTH_METRICS(ignore));
-        {error, timeout} ->
-            ?LOG(error, "[MongoDB] Can't connect to MongoDB server: timeout"),
-            ok = emqx_metrics:inc(?AUTH_METRICS(failure)),
-            {stop, AuthResult#{auth_result => not_authorized, anonymous => false}};
         {error, Reason} ->
             ?LOG(error, "[MongoDB] Can't connect to MongoDB server: ~0p", [Reason]),
             ok = emqx_metrics:inc(?AUTH_METRICS(failure)),
